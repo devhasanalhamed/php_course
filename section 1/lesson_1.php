cc127aa6118aef561742986f8e935ce18301a3f1
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-23 01:29:25 +0300

    Create lesson_1.php

---
+++ section 1/lesson_1.php
@@ -0,0 +1,28 @@
+<?php 
+echo "Hello World!";
+echo "<br>";
+echo "This is my first PHP code";
+echo "<br>";
+echo "PHP files starts with <?php ?> tags and could have no end";
+echo "<br>";
+echo "While the line statement is not the last one we should add the semi-colon";
+echo "<br>";
+echo "We can print either using (echo) or (print)";
+echo "<br>";
+ECHo "Php is not Case-Sensitive";
+eCHO "<br>";
+
+?>
+
+<?php
+print"php support multiple code section using tag php";
+echo "<br>";
+print 'also support short hand echo statement (< ?= "statement" >)';
+echo "<br>";
+
+?>
+
+<?= "Hello World! Again!!"?>
+
+<!-- The best practice is not to add end tag when the php is last section at any code,
+  because it may contain spaces and added it to buffer -->
