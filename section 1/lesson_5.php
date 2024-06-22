445214a73e672c0760ba5e72c9168ddea9c7ca3c
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-23 01:29:37 +0300

    Create lesson_5.php

---
+++ section 1/lesson_5.php
@@ -0,0 +1,27 @@
+<?php
+
+// Type casting
+/*
+"boolean" or "bool"
+"integer" or "int"
+"float" or "double" or "real"
+"string"
+"array"
+"object"
+"null"
+*/
+
+echo 5 + (int) "5 Lessons";
+echo "<br>";
+echo 5 + (integer) "5 Lessons";
+echo "<br>";
+echo 5 + (INTEGer) "5 Lessons";
+echo "<br>";
+echo 5.1 + (double) "5 Lessons";
+echo "<br>";
+echo 5 + (int) 10.5;
+echo "<br>";
+echo 5 + (int) "5 Lessons";
+echo "<br>";
+
+// 9
