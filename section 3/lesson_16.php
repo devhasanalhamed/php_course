49161d8958f6ebc9a0d51231117adbeb418649a6
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-25 15:17:39 +0300

    Create lesson_16.php

---
+++ section 3/lesson_16.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+Operators
+used to preform operations on values.
+
+Arithmetic operators
+used to do arithmetical operations and conversion
+
+$a [+] $b => Addition
+$a [-] $b => Subtraction
+$a [*] $b => Multiplication
+$a [/] $b => Division
+$a [%] $b => Modulus
+$a [**] $b => Exponentiation
++$a => Identity
+-$a => Negation
+ */
+
+echo "10 + 20 = ";
+echo 10 + 20;
+echo "<br>";
+echo gettype(10 + 20);
+echo "<br>";
+
+echo "9.5 + 19.5 = ";
+echo 9.5 + 19.5;
+echo "<br>";
+echo gettype(9.5 + 19.5);
+echo "<br>";
+
+echo "<hr>";
+
+echo "10 - 20 = ";
+echo 10 - 20;
+echo "<br>";
+echo gettype(10 - 20);
+echo "<br>";
+
+echo "9.5 - 19.5 = ";
+echo 9.5 - 19.5;
+echo "<br>";
+echo gettype(9.5 - 19.5);
+echo "<br>";
+
+echo "<hr>";
+
+echo "10 * 20 = ";
+echo 10 * 20;
+echo "<br>";
+echo gettype(10 * 20);
+echo "<br>";
+
+echo "9.5 * 19.5 = ";
+echo 9.5 * 19.5;
+echo "<br>";
+echo gettype(9.5 * 19.5);
+echo "<br>";
+
+echo "<hr>";
+
+echo "100 / 20 = ";
+echo 100 / 20;
+echo "<br>";
+echo gettype(100 / 20);
+echo "<br>";
+
+echo "9.5 / 19.5 = ";
+echo 9.5 / 19.5;
+echo "<br>";
+echo gettype(9.5 / 19.5);
+echo "<br>";
+
+echo "<hr>";
+
+echo "21 % 20 = ";
+echo 21 % 20;
+echo "<br>";
+echo gettype(21 % 20);
+echo "<br>";
+
+echo "110 % 20 = ";
+echo 110 % 20;
+echo "<br>";
+echo gettype(110 % 20);
+echo "<br>";
+
+echo "<hr>";
+
+echo "2 ** 4 = ";
+echo 2 ** 4;
+echo "<br>";
+echo gettype(2 ** 4);
+echo "<br>";
+
+echo "10 ** 5 = ";
+echo 10 ** 5;
+echo "<br>";
+echo gettype(10 ** 5);
+echo "<br>";
+
+echo "<hr>";
+
+// turn strings to numbers like unary in javascript
+echo "+\"12\" = ";
+echo +"12";
+echo "<br>";
+echo gettype(+"12");
+echo "<br>";
+
+echo "-\"3\" = ";
+echo -"3";
+echo "<br>";
+echo gettype(-"3");
+echo "<br>";
+
+echo "<hr>";
