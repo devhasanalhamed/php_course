46ce7bbc604d32448c77c31f123cd16bc8c132a9
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-26 17:08:24 +0300

    Create lesson_17.php

---
+++ section 4/lesson_17.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Operators
+
+Assignment Operators
+write value to another
+
+$a [+=] $b => Addition
+$a [-=] $b => Subtraction
+$a [/=] $b => Multiplication
+$a [/=] $b => Division
+$a [%=] $b => Modulus
+$a [**=] $b => Exponentiation
+ */
+
+ $a = 10;
+$b = 10;
+
+$a += 20;
+
+$b += 20;
+
+echo"$a";
+echo "$b";
