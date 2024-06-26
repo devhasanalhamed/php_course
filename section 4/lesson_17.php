f1620ca99d37b3f6cd37cd68c3e2b7937ddb302e
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-27 01:15:58 +0300

    Update lesson_17.php

--- section 4/lesson_17.php
+++ section 4/lesson_17.php
@@ -14,12 +14,45 @@ $a [%=] $b => Modulus
 $a [**=] $b => Exponentiation
  */
 
- $a = 10;
-$b = 10;
+$a = 10;
 
 $a += 20;
 
-$b += 20;
+echo "a(10) += 20: ";
+echo "$a";
+echo "<br>";
 
-echo"$a";
+$b = 20;
+$b -= $a;
+
+echo "b(20) -= a: ";
 echo "$b";
+echo "<br>";
+
+$c = 30;
+
+$c *= $b;
+echo "c(30) *= b: ";
+echo "$c";
+echo "<br>";
+
+$d = 40;
+
+$d /= 5;
+echo "d(40) /= 5: ";
+echo "$d";
+echo "<br>";
+
+$f = 3;
+
+$f **= $f;
+echo "f(3) **= f: ";
+echo "$f";
+echo "<br>";
+
+$g = 21;
+
+$g %= 5;
+echo "g(21) %= 5: ";
+echo "$g";
+echo "<br>";
