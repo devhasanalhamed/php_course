337340fa656724cdb077b49b441d3402041aae00
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-30 00:32:39 +0200

    Create lesson_26.php

---
+++ section 6/lesson_26.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Control structure
+    - If, Else if, Else => Basics
+
+Syntax
+    if (condition) {
+        code to be executed if condition is true
+    }
+*/
+
+if (10 < 6) {
+    echo "First condition";
+} else if (1 > 2) {
+    echo "Second condition";
+} else {
+    echo "No satisfying condition";
+}
