6d47ab91f42065483a8bfce66fc1d6eadcca2fa0
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 02:03:14 +0300

    Update lesson_2.php

--- section 1/lesson_2.php
+++ section 1/lesson_2.php
@@ -1,14 +1,14 @@
 <?php
 
-print"comments in php";
+print "comments in php";
 // single line 
 /*
 Multiline
 */
 
-print"<br>";
-print"1- //";
-print"<br>";
-print"2- /**/";
-print"<br>";
-print"3- //";
+print "<br>";
+print "1- //";
+print "<br>";
+print "2- /**/";
+print "<br>";
+print "3- #";
