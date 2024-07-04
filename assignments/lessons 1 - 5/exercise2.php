533a88e080a5573d024478b73a04588c30b70fb1
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 02:32:54 +0300

    Create exercise2.php

---
+++ assignments/lessons 1 - 5/exercise2.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+التكليف 02
+
+    إستخدم التعليقات في كل من الأسطر التالية لمنع تشغيل ال Code
+    يجب إستخدام نوع مختلف من التعليقات مع كل سطر
+
+ */
+
+// echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+# echo 'Prevent Me From Running Please';
