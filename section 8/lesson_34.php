70d355a5768ae6760287c3f54a4cd4c46dd47441
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-04 22:19:50 +0300

    Create lesson_34.php

---
+++ section 8/lesson_34.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Control structure
+- do while
+will run the block is code minimally once, then check condition.
+ */
+
+
+$i = 5;
+
+do {
+    echo "Hello $i <br>";
+    $i += 1;
+} while ($i <= 3);
