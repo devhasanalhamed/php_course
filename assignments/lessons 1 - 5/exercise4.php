28fc2dabcba87be2fd63ded7855b6e33d8c7d28e
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 02:33:00 +0300

    Create exercise4.php

---
+++ assignments/lessons 1 - 5/exercise4.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+التكليف 04
+
+    أي من التعليقات التالية سليمة وأيها خطأ
+
+ */
+
+ // ## First Comment // ✔️
+# // # Second Comment // ✔️
+/* /* /* Third Comment */ // ✔️
+////// Fourth Comment  // ❌
