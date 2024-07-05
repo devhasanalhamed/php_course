24989fb5387a2434dd20f444c2afc32540f8034f
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 03:50:41 +0300

    Create exercise6.php

---
+++ assignments/lessons 13 - 19/exercise6.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+التكليف 05
+
+    إنشيء Multiple Lines Comment
+    اكتب بداخله 10 كلمات من الكلمات المحجوزة في اللغة والتي لا يمكن إستخدمها في برمجياتك
+
+ */
+
+ /*
+break, do, clone,
+for, while, switch, 
+function, continue, else,
+or, and
+  */
