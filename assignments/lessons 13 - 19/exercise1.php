e1e626fc8ce6b8a3d3fe12230d0b8ec870a5987a
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 03:50:28 +0300

    Create exercise1.php

---
+++ assignments/lessons 13 - 19/exercise1.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+التكليف 01
+
+    لديك ال Code التالي يحتوي على صفحة كاملة
+    إستخدم ما تعلمته في المتغيرات لنستطيع تغيير جملة Elzero Courses  مستقبلا
+
+ */
+
+$courses = "Elzero Courses";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Elzero Courses">
+    <title>Welcome To Elzero Courses</title>
+</head>
+
+<body>
+    <h1>Elzero Courses</h1>
+    <p>Here In <?php echo $courses ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $courses ?> Is The What You Need.</div>
+    <footer>All Right Reserved To Elzero Courses</footer>
+</body>
+
+</html>
