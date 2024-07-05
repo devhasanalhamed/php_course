474a18c93624ca2ebd3930d6a9bae6c943d9bd51
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-05 03:12:00 +0300

    Create exercise7.php

---
+++ assignments/lesson 6 - 12/exercise7.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+التكليف 07
+
+    لديك ال Code التالي بالأسفل
+    يجب عدم التعديل على الجملة نهائيا ولا نوعها
+    المطلوب إستخدام ما تعلمته ليخرج ال Output في سطر Echo الأول رقم 1 وفي سطر Echo الثاني كلمة integer
+
+ */
+
+echo (int) (bool) "Hello PHP";
+echo '<br>';
+echo gettype((int) (bool)"Hello PHP");
+
+/* Needed Output
+1
+integer
+*/
