9cd968b299afa15f407f69c41075449786554317
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-06 23:32:52 +0300

    Create exercise3.php

---
+++ assignments/lessons 43 - 52/exercise3.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+التكليف 03
+
+    قم بإنشاء Function تقوم بجمع كل الأرقام التي تعطى لها ك Arguments
+    إذا وجدت في الأرقام رقم 5 لا تجمعه مع الأرقام
+    إذا وجدت في الأرقام رقم 10 غير قيمته إلى 20 وأكمل العملية الحسابية كما هي
+    شاهد المثال لتفهم الفكرة
+
+ */
+
+function sum_all(...$numbers)
+{
+
+    $sum = 0;
+
+    foreach ($numbers as $number) {
+        if ($number == 5) {
+            continue;
+        } elseif ($number == 10) {
+            $sum += 20;
+        } else {
+            $sum += $number;
+        }
+    }
+    return $sum;
+}
+
+echo sum_all(10, 12, 5, 6, 6, 10) . "<br>"; // 64
+echo sum_all(5, 10, 5, 10) . "<br>"; // 40
+
+
+/* 
+Needed Output
+echo sum_all(10, 12, 5, 6, 6, 10); // 64
+echo sum_all(5, 10, 5, 10); // 40
+*/
