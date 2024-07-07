0a667fbaeef16bdf84cf5815fc4728fb6ecc75f7
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-07 20:41:36 +0300

    Create exercise7.php

---
+++ assignments/lessons 43 - 52/exercise7.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+التكليف 07
+
+    لديك Function تقوم بعمل جمع لرقمين
+    المطلوب عدم التعديل على نوع الأرقام الموجودة في ال Parameters ولا الموجودة في ال Return
+    يمكنك التعديل على ال Function نفسها للتأكد أن الرقم الذي سيرجع نوعه Double وليس Integer
+    شاهد المثال لتفهم الفكرة
+
+ */
+
+function calculate(int $num_one, int $num_two): float
+{
+    return $num_one + $num_two;
+}
+
+echo calculate(20, 10) . "<br>"; // 30
+echo gettype(calculate(20, 10)) . "<br>"; // Double
