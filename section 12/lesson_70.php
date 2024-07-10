819aefe340a93dde76172e2b0865371277d8ca94
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-10 14:49:18 +0300

    Create lesson_70.php

---
+++ section 12/lesson_70.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Math functions
+
+- ceil(Number[Required])
+    - round up to integer
+
+- floor
+    - round down to integer
+ */
+
+
+$a = 3.2;
+$b = 3.6;
+
+$c = -2.4;
+
+echo ceil($a) . "<br>";
+echo ceil($b) . "<br>";
+
+echo floor($a) . "<br>";
+echo floor($b) . "<br>";
+
+echo ceil($c) . "<br>";
+echo floor($c) . "<br>";
