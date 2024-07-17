4fa57aca32ccaa6399ef24f695c6c95651d57caf
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-17 21:59:49 +0300

    Create exercise7.php

---
+++ assignments/lessons 53 - 62/exercise7.php
@@ -0,0 +1,5 @@
+<?php
+
+/*
+Set-VHD "D:\Hyper-V\Windows 11\Virtual Hard Disks\Windows 11_A2245C39-04F7-4799-9F6C-FE519881FEC5.avhdx" -ParentPath "D:\Hyper-V\Windows 11\Virtual Hard Disks\Windows 11.vhdx" -IgnoreIDMismatch
+ */
