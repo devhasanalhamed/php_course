de516f4a8881a1741df69e5379a29e5c9f1f1435
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-19 13:23:51 +0300

    Update exercise7.php

--- assignments/lessons 53 - 62/exercise7.php
+++ assignments/lessons 53 - 62/exercise7.php
@@ -1,4 +1,24 @@
 <?php
 
 /*
+التكليف 07 تحدي
+
+    لديك ال Array التالية تحتوي على أرقام وحروف
+    لا نحتاج للأرقام في طباعة النتيجة
+    إستخدم ما تعلمته لتخرج بالقيمة المطلوبة “Elzero”
+
+ */
+
+$chars = ["E", 1, 2, "l", "z", "E", "R", "o"];
+
+$result = $chars[0] . substr(strtolower(implode($chars)), 3);
+
+echo  $result . "<br>";
+
+
+
+
+ /* 
+ Output
+ "Elzero"
  */
