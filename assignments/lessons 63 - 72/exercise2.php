b52e61cbb25ad6de6df9f1291f139db9e7092498
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-20 14:00:53 +0300

    Create exercise2.php

---
+++ assignments/lessons 63 - 72/exercise2.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+التكليف 02
+
+    لديك Two Arrays
+    المطلوب إستخدام ال Array Built-In Functions لتخرج بال Output المطلوب
+
+ */
+
+$codes = ["H", "C", "J"];
+$means = ["HTML", "CSS", "JavaScript"];
+
+echo "<pre>";
+print_r(array_change_key_case(array_combine($codes, $means)));
+echo "</pre>";
+/* 
+Output
+ Array
+ (
+   [h] => HTML
+   [c] => CSS
+   [j] => JavaScript
+ )
+*/
