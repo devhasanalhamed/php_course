6f233fce236bc2472ba263a0692ed731eb64c8ea
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-20 14:00:49 +0300

    Create exercise1.php

---
+++ assignments/lessons 63 - 72/exercise1.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+التكليف 01
+
+    لديك ال Array التالية
+    المطلوب إستخدام ال Array Built-In Functions لتخرج بال Output المطلوب
+
+ */
+
+$friends = [
+    "AG" => "Ahmed Gamal",
+    "OM" => "Osama Mohamed",
+    "MG" => "Mahmoud Gamal",
+    "AS" => "Ahmed Samy",
+    "FA" => "Farid Ahmed",
+    "SM" => "Sayed Mohamed"
+];
+
+
+
+echo "<pre>";
+print_r(array_chunk(array_change_key_case($friends, CASE_LOWER), 2, true));
+echo "</pre>";
+  
+/* 
+Output
+  Array
+  (
+    [0] => Array
+      (
+        [ag] => Ahmed Gamal
+        [om] => Osama Mohamed
+      )
+    [1] => Array
+      (
+        [mg] => Mahmoud Gamal
+        [as] => Ahmed Samy
+      )
+    [2] => Array
+      (
+        [fa] => Farid Ahmed
+        [sm] => Sayed Mohamed
+      )
+  )
+*/
