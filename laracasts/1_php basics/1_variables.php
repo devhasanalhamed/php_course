af21ee62b13dc0ad6b2aaae89c42fd346b80d96d
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-18 07:49:08 +0300

    Create 1_variables.php

---
+++ laracasts/1_php basics/1_variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>variables</title>
+</head>
+
+<body>
+    <?php
+    // define variables
+    $greetings = "Hello,";
+    $name = "Hamed";
+
+    // print the output
+    echo "Hello World! (echo)";
+    echo '<br>';
+    print "Hello World! (print)";
+    echo '<br>';
+
+    // string concatenation
+    echo $greetings . " Hassan";
+    echo '<br>';
+
+    // string interpolation
+    echo "$greetings $name";
+    echo '<br>';
+    ?>
+</body>
+
+</html>
