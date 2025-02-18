cfa519f3c940f94658a7cfab07606e46db1b01c7
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-18 07:49:34 +0300

    Create 8_technical_check_in.php

---
+++ laracasts/1_php basics/8_technical_check_in.php
@@ -0,0 +1,10 @@
+<?php
+
+$ramzHub = [
+    'name' => 'RamzHub',
+    'description' => 'RamzHub is a platform that provides free courses for developers.',
+    'categories' => ['PHP', 'Dart', 'Flutter', 'Laravel', 'Find more'],
+];
+
+
+require '8_technical_check_in.view.php';
