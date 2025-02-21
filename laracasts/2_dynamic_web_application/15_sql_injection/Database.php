c71cbaffea1bde049295974fcecb5c5897edcdba
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-21 10:07:20 +0300

    Create Database.php

---
+++ laracasts/2_dynamic_web_application/15_sql_injection/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+// File name starts with a capital letter if it's only contains one class
+
+class Database
+{
+    private $connection;
+
+
+    public function __construct($config, $user = 'root', $password = '')
+    {
+
+
+
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $user, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+    }
+
+
+    public function execute($query, $parameters)
+    {
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute($parameters);
+
+        return  $statement;
+    }
+}
