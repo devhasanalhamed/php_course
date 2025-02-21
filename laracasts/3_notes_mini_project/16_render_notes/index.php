21b28bce72c0ce59360b2d78fbe38a5328cfce2b
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-21 10:33:09 +0300

    Update index.php

--- laracasts/3_notes_mini_project/16_render_notes/index.php
+++ laracasts/3_notes_mini_project/16_render_notes/index.php
@@ -4,26 +4,4 @@ require('functions.php');
 
 require 'Database.php';
 
-// require 'router.php';
-
-
-$config = require('config.php');
-
-$db = new Database($config['database'], 'root', '1234');
-
-// To solve the problem of sql injection there is a general rule that we should never trust the user input
-// do not ever inline the user input in the query
-// other way is to bind the parameters to the query by run the query with the parameters
-// $query = "select * from posts where id = :id"; or $query = "select * from posts where id = ?";
-
-$query = "select * from posts where id = ?";
-
-$id = $_GET['id'];
-
-
-// if you used :id in the query you should pass ['id' => $id] or [':id' => $id]
-$posts = $db->execute($query, [$id])->fetchAll();
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+require 'router.php';
