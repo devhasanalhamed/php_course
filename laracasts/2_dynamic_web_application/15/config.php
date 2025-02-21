dbc5f548558b56ec512d5b00a5749c39ec65e69f
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-21 08:53:40 +0300

    Create config.php

---
+++ laracasts/2_dynamic_web_application/15/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return  [
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'port' => 3306,
+        'dbname' => 'laracasts',
+        'charset' => 'utf8mb4',
+    ],
+];
